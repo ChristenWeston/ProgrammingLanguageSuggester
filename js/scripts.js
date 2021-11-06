1ee0e7e7eb5888d29c9adc0cddbba0396b267b98
Christen Weston <paleeserecycle@gmail.com>
2021-11-06 12:14:11 -0700

    Added in additional questions and updated scripts.js to store the results from those questions

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,18 @@ $(document).ready(function() {
     //Just for testing animal score value
     alert(animalScore1);
 
+    const soulScore1 = $("#soul").val();
+    alert(soulScore1);
+
+    const potatoScore1 = $("#favorite-potato").val();
+    alert(potatoScore1);
+
+    //When your born doesn't affect the score
+    const birthdayScore1 = $("#birthday").val();
+    alert("This is your birthday score" + birthdayScore1);
+
+    const bestSongScore1 = $("#best-song").val();
+    alert("This is your song score" + bestSongScore1);
 
     $("#quiz-result").show();
     $("#quiz").hide();
