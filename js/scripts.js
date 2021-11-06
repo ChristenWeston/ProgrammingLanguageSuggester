1d3ff61854520936ba89b116fb0e81e302cde393
Christen Weston <paleeserecycle@gmail.com>
2021-11-05 18:53:22 -0700

    Added in a radio button question for cutest animal. Also added in a constant for first question and an alert to test it in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,7 @@ $(document).ready(function() {
   $("form#quiz").submit(function() {
     event.preventDefault();
 
-  )};
+    const animalScore1 = parseInt($("#question1").val());
+    alert(animalScore1);
+  });
 });
