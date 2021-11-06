71df9780ff9346b25cb282ea81640bcbb135ff60
Christen Weston <paleeserecycle@gmail.com>
2021-11-06 13:50:49 -0700

    Changed result to be a card with an image

--- js/scripts.js
+++ js/scripts.js
@@ -3,26 +3,16 @@ $(document).ready(function() {
   $("#quiz-result2").hide();
   $("#quiz-result3").hide();
   $("#quiz-result4").hide();
+  $("#quiz-result5").hide();
   $("form#quiz").submit(function(event) {
     event.preventDefault();
 
     const animalScore1 = parseInt($("input:radio[name=animal]:checked").val());
-    //Just for testing animal score value
-    alert(animalScore1);
-
     const soulScore1 = parseInt($("#soul").val());
-    alert(soulScore1);
-
     const potatoScore1 = parseInt($("#favorite-potato").val());
-    alert(potatoScore1);
-
     //When your born doesn't affect the score
     const birthdayScore1 = $("#birthday").val();
-    alert("This is your birthday score" + birthdayScore1);
-
     const bestSongScore1 = parseInt($("#best-song").val());
-    alert("This is your song score" + bestSongScore1);
-
     const totalScore = animalScore1 + soulScore1 + potatoScore1 + bestSongScore1;
     alert("This is your total score (Max: 20 Min:3) :" + totalScore);
 
@@ -36,7 +26,7 @@ $(document).ready(function() {
     } else if (totalScore <=10 && totalScore > 3) {
       result = $("#quiz-result4").show();
     } else if (totalScore === 3) {
-      result = Go;
+      result = $("#quiz-result5").show();
     }
 
     //Show result
