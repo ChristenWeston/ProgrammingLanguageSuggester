bfb6a5fa646764be51e430914f00fd3d47dd4927
Christen Weston <paleeserecycle@gmail.com>
2021-11-06 12:45:19 -0700

    Updated scripts.js to parse all scores into integers and then add up the scores to a total

--- js/scripts.js
+++ js/scripts.js
@@ -3,22 +3,27 @@ $(document).ready(function() {
   $("form#quiz").submit(function(event) {
     event.preventDefault();
 
-    const animalScore1 = $("input:radio[name=animal]:checked").val();
+    const animalScore1 = parseInt($("input:radio[name=animal]:checked").val());
     //Just for testing animal score value
     alert(animalScore1);
 
-    const soulScore1 = $("#soul").val();
+    const soulScore1 = parseInt($("#soul").val());
     alert(soulScore1);
 
-    const potatoScore1 = $("#favorite-potato").val();
+    const potatoScore1 = parseInt($("#favorite-potato").val());
     alert(potatoScore1);
 
     //When your born doesn't affect the score
     const birthdayScore1 = $("#birthday").val();
     alert("This is your birthday score" + birthdayScore1);
 
-    const bestSongScore1 = $("#best-song").val();
+    const bestSongScore1 = parseInt($("#best-song").val());
     alert("This is your song score" + bestSongScore1);
+
+    const totalScore = animalScore1 + soulScore1 + potatoScore1 + bestSongScore1;
+    alert("This is your total score" + totalScore);
+    
+    let result;
 
     $("#quiz-result").show();
     $("#quiz").hide();
